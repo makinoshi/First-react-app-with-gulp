65a52f826aa89a5d3811d19d0324a7b49f966270
makinoshi <masa.kinoshita02@gmail.com>
2015-05-21 20:54:25 +0900

    modifiy syntax

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ var React = require('react');
 
 var view = require('./view.jsx');
 
-React.render(
-  React.createElement(view),
-  document.getElementById('content')
-);
+(function() {
+  'use strict';
+  
+  React.render(
+    React.createElement(view),
+    document.getElementById('content')
+  );
+})();
